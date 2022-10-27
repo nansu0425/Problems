da05cf01bb95776e36fe1f522813d625332de6d9
nansu0425 <nansu0425@gmail.com>
2022-10-27 21:25:11 +0900

    add 문자열 조작 49, 819, 937

---
+++ LeetCode/파이썬 알고리즘 인터뷰/문자열 조작/819. Most Common Word.py
@@ -0,0 +1,6 @@
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        words = [word for word in re.sub(r'[^\w]', ' ', paragraph).lower().split() if word not in banned]
+        
+        words_counter = collections.Counter(words)
+        return words_counter.most_common(1)[0][0]
